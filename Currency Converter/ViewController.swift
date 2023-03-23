027e891b5ae456ec4e0dd4cf1cdaf97e0e189981
AndUser <anduser@PLKRK-J646Y59N91.local>
2023-03-23 14:57:48 +0100

    APP-5277: Currency Conversion task

---
+++ Currency Converter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Currency Converter
+//
+//  Created by AndUser on 23/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
