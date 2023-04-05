37030afed13a966a1d04b89da7ee90ebd39e4676
AndUser <anduser@PLKRK-J646Y59N91.local>
2023-04-05 11:32:21 +0200

    Added common rawRepresentable EndPoint enum

---
+++ Currency Converter/Services/Helpers/EndPoint.swift
@@ -0,0 +1,40 @@
+//
+//  EndPoint.swift
+//  Currency Converter
+//
+//  Created by AndUser on 05/04/2023.
+//
+
+import Foundation
+
+protocol API {
+    static var baseURL: String { get }
+}
+
+protocol RawRepresentable {
+    associatedtype RawValue
+    init?(rawValue: Self.RawValue)
+    var route: Self.RawValue { get }
+}
+
+extension RawRepresentable where RawValue == String, Self: API {
+    var url: String { Self.baseURL + route }
+    
+    init?(rawValue: String) { nil }
+}
+
+enum EndPoint: RawRepresentable, API {
+    init?(rawValue: String) {
+        nil
+    }
+    
+    static let baseURL = "http://api.evp.lt"
+    
+    var route: String {
+        switch self {
+        case .exchange(let fromAmount, let fromCurrency, let toCurrency): return "/currency/commercial/exchange/{\(fromAmount)}-{\(fromCurrency)}/{\(toCurrency)}/latest"
+        }
+    }
+    
+    case exchange(fromAmount: Double, fromCurrency: String, toCurrency: String)
+}
